94828f44261f58552c25e8e957b71ca42212920b
rakeshcooper <rakeshmurali097@gmail.com>
2024-07-26 00:33:39 +0530

    new update

--- index.js
+++ index.js
@@ -2,17 +2,16 @@ let rId
 let modeTypes = JSON.parse(localStorage.getItem("NData") ?? "[]")
 let mode = document.querySelector(".mode")
 let services = document.querySelector(".service")
-let remove = document.querySelectorAll(".close")
 render()
 
 // To select normal note or Checkbox note
 mode.addEventListener("click",(e)=>{
     if(e.target.classList.contains("nNote")){
-        let Nlist = `<li><div><p>title</p><span class="close">X</span><textarea rows="4" cols="50"></textarea></div></li>`
+        let Nlist = `<li><p>title</p><span class="close">X</span><textarea rows="4" cols="50"></textarea></li>`
         createElement(Nlist)
         console.log("normal note");
     } else if(e.target.classList.contains("cNote")){
-        let Clist =  `<li><div><p>title</p><span class="close">X</span><input type="todo" name="todo" id="todo"> <button>Add Task</button></div></li>`
+        let Clist =  `<li><p>title</p><span class="close">X</span><input type="todo" name="todo" id="todo"> <button>Add Task</button></li>`
         createElement(Clist)
         console.log("checkbox note");
     } 
@@ -20,7 +19,7 @@ mode.addEventListener("click",(e)=>{
     localStorage.setItem("NData",JSON.stringify(modeTypes))
     console.log("active");
 })
-
+let remove = document.querySelectorAll(".close")
 
 // This function is used to create element and pushed those element to array 
 function createElement(list){
@@ -33,6 +32,19 @@ function createElement(list){
     console.log(modeTypes);
 }
 
+
+//To remove the elements
+remove.forEach((element) => {
+    element.addEventListener("click", ()=> {
+        console.log("removed");
+        element.parentElement.remove()
+
+    })
+    
+})
+
+console.log(modeTypes);
+
 // This function used to render arrays from local storage
 function render(){
     modeTypes.forEach((element) => {
@@ -41,14 +53,5 @@ function render(){
         services.append(newElement.firstChild) 
     })
 }
-
-//To remove the elements
-remove.forEach((element) => {
-    element.addEventListener("click", ()=> {
-        console.log("removed");
-    })
-})
-
-// console.log(typeof services.children[0]);
 
 
